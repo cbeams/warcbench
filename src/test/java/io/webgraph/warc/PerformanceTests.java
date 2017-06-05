271be44042423ede91ce605b1e0acd52fac6fd5b
Chris Beams <chris@beams.io>
2017-06-05 11:47:32 +0200

    Test that LFS-managed files are actually present

---
+++ src/test/java/io/webgraph/warc/PerformanceTests.java
@@ -0,0 +1,16 @@
+package io.webgraph.warc;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PerformanceTests {
+
+    @Test
+    public void test() {
+        assertThat(new File(getClass().getResource("/foo.warc").getFile())).exists();
+        assertThat(new File(getClass().getResource("/foo.warc.gz").getFile())).exists();
+    }
+}
